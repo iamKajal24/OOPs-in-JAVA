56b3ee3398c6b4712d52273193db1118d0920c48
Kajal <113601134+iamKajal24@users.noreply.github.com>
2023-02-13 20:13:54 +0530

    Add files via upload

---
+++ class_object.java
@@ -0,0 +1,22 @@
+package oops;
+
+class employee {
+	public int empid;
+	public String name;
+	public int salary;
+
+}
+
+public class class_object {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		employee e = new employee();
+		e.empid = (102);
+		e.name = ("kajal");
+		e.salary = (5000);
+		System.out.println(e.empid);
+		System.out.println(e.name);
+		System.out.println(e.salary);
+	}
+}
